1a7e5db0139d0813c9033da0372ddcebad302cc5
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-02-28 15:42:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+//Task 11
+// Assignment solution
+//Names:Store the names of a few of your friends in ab arry called
+// names.Print each person's name by accessing each element in the 
+//list.one at a time.
+var names = ["Muhammad", "Ali", "Essa", "Hamza"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ app.ts
@@ -0,0 +1,10 @@
+//Task 11
+// Assignment solution
+//Names:Store the names of a few of your friends in ab arry called
+// names.Print each person's name by accessing each element in the 
+//list.one at a time.
+let names: string [] = ["Muhammad", "Ali", "Essa", "Hamza"];
+for(let i=0; i<names.length; i++){
+console.log(names[i]);
+
+}
